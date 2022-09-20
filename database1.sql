805174ae8201a092b1588a4447ead07a17d4a88c
eduarddanaila <eduardanaila04@gmail.com>
2022-09-20 16:41:21 +0100

    database2

--- database1.sql
+++ database1.sql
@@ -7,3 +7,12 @@ cust_id INT UNIQUE NOT NULL AUTO_INCREMENT,
 cust_name VARCHAR(100) NOT NULL,
 phone_no CHAR(11) NOT NULL,
 PRIMARY KEY(cust_id));
+
+SHOW TABLES;
+
+describe customers;
+
+INSERT INTO customers(cust_name, phone_no) VALUES("ed", "12345678901");
+
+SELECT * FROM customers;
+
